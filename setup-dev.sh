f21799be024bb9265b728e009fae237cb2b678ab
Arturo <baldoarturo@gmail.com>
2025-04-23 12:47:41 -0300

    podman fix

--- setup-dev.sh
+++ setup-dev.sh
@@ -5,7 +5,7 @@ python3 python3-pip python3-venv python3-dev \
 build-essential libxml2-dev libxslt1-dev libffi-dev libpq-dev libssl-dev zlib1g-dev
 sudo apt install -y podman
 pip3 install podman-compose
+podman-compose -f compose.yml up -d
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-podman-compose up -d -f compose.yml
